49f8aeda8d54f210df61c5393cbeda54482cd7ef
Kalamon9 <kmonnier@hotmail.com>
2022-11-01 17:20:35 +0000

    add keydown

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
+const message = document.querySelector(".message");
+const modal = document.getElementById("modal");
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(`my secret number is ${secretNumber}`);
+
+let score = 20;
+let highscore = 0;
+
+function displayMessage(messageContent) {
+  message.textContent = messageContent;
+}
+
 function errorMessage() {
-  document.querySelector(".message").style.color = "rgb(255, 2, 192)";
-  document.querySelector(".message").style.transition = "all 1s";
+  message.style.color = "rgb(255, 2, 192)";
+  message.style.transition = "color 1s";
   setTimeout(function () {
-    document.querySelector(".message").style.color = "white";
+    message.style.color = "white";
   }, 3000);
 }
 
 function togglePopup() {
-  document.getElementById("modal").classList.toggle("active");
+  modal.classList.toggle("active");
+  modal.style.transition = "all .3s";
 }
+modal.addEventListener("click", function () {
+  modal.classList.remove("active");
+});
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(`my secret number ${secretNumber}`);
-
-let score = 20;
-let highscore = 0;
-function displayMessage(message) {
-  document.querySelector(".message").textContent = message;
-}
-
-document.querySelector(".check").addEventListener("click", function () {
+function checkNo() {
   const guess = Number(document.querySelector(".guess").value);
-  // console.log(guess);
-
   if (!guess) {
     displayMessage("No number was selected!");
     errorMessage();
@@ -31,7 +37,7 @@ document.querySelector(".check").addEventListener("click", function () {
     // document.querySelector(".message").style.color = "rgb(26, 179, 166)";
     document.querySelector(".number").style.width = "15rem";
     document.querySelector(".number").textContent = secretNumber;
-    document.getElementById("modal").classList.toggle("active");
+    togglePopup();
 
     if (highscore < score) {
       highscore = score;
@@ -52,13 +58,24 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+}
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Enter") {
+    checkNo();
+  }
+});
+
+document.querySelector(".check").addEventListener("click", function () {
+  checkNo();
 });
 
 //again
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(`new secret number ${secretNumber}`);
+  console.log(`my new secret number is ${secretNumber}`);
   displayMessage("Start again");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
